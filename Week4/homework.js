cf459897e1967915f9dff8597cb975867429bc14
Taylor <taylorbarnett281@yahoo.com>
2020-06-13 13:43:59 -0500

    Completed Home Work 1

--- Week4/homework.js
+++ Week4/homework.js
@@ -1,10 +1,21 @@
 //create a function name myFilter
 //that takes an array and filters by conditon
-funcion myFilter(myArray, callBack){
-
-}
-
+// funcion myFilter(myArray, callBack){
+    
+// }
 let names = ["homer", "bart", "marge", "list"]
+console.log(names.sort(function(a,b){
+    let x = a.toLocaleLowerCase()
+        y = b.toLocaleLowerCase()
+    if(x < y){
+        return -1
+    }
+    if(y < x){
+        return 1
+    }
+    return 0
+}))
+
 
 let filteredNames = myFilter(names, 
     function(item){
@@ -15,4 +26,4 @@ let ages = [11,13,30,42]
 
 let evenAges = myFilter(ages, function(item){
     item % 2 == 0;
-})
+})      
